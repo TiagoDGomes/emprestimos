58c9b0c1459d99af515543bb74ae16655f4f4c88
Tiago Gomes <tiago.gomes@ifsp.edu.br>
2020-03-13 14:49:43 -0300

    inversão no testes de igualdade

--- core/tests/tests_busca.py
+++ core/tests/tests_busca.py
@@ -33,10 +33,10 @@ class TestBuscaPadrao(TestCaseBase):
     def test_busca_itens_emprestados_pessoa(self):
         reservas = self.sala.pedir_agora(self.pessoa, self.amanha_hora_1)
         response = self.busca_pessoa()
-        self.assertEqual(response['pessoas'][0]['reservas'][0]['item'], self.sala.nome)
-        self.assertEqual(response['pessoas'][0]['reservas'][0]['id'], reservas[0].id)
-        self.assertEqual(response['pessoas'][0]['reservas'][0]['item_id'], self.sala.id)
-        self.assertEqual(response['pessoas'][0]['reservas'][0]['data_hora_fim'], reservas[0].data_hora_fim.isoformat())
+        self.assertEqual(self.sala.nome, response['pessoas'][0]['reservas'][0]['item'])
+        self.assertEqual(reservas[0].id, response['pessoas'][0]['reservas'][0]['id'], )
+        self.assertEqual(self.sala.id, response['pessoas'][0]['reservas'][0]['item_id'])
+        self.assertEqual(reservas[0].data_hora_fim.isoformat(), response['pessoas'][0]['reservas'][0]['data_hora_fim'])
 
 
     def test_busca_pessoa_bloqueada(self):
@@ -53,33 +53,33 @@ class TestBuscaPadrao(TestCaseBase):
 
     def test_busca_pessoa_matricula(self):
         response = self.busca(self.pessoa.matricula)
-        self.assertEqual(response['pessoas'][0]['nome'], self.pessoa.nome)
+        self.assertEqual(self.pessoa.nome, response['pessoas'][0]['nome'])
 
     def test_busca_pessoa_cpf(self):
         response = self.busca(self.pessoa.cpf)
-        self.assertEqual(response['pessoas'][0]['nome'], self.pessoa.nome)
+        self.assertEqual(self.pessoa.nome, response['pessoas'][0]['nome'])
 
     def test_busca_pessoa_cpf_part(self):
         response = self.busca(self.pessoa.cpf[1:3])
-        self.assertEqual(len(response['pessoas']), 0)
+        self.assertEqual(0, len(response['pessoas']))
 
     def test_busca_pessoa_matricula_part(self):
         response = self.busca(self.pessoa.matricula[1:3])
-        self.assertEqual(len(response['pessoas']), 0)
+        self.assertEqual(0, len(response['pessoas']))
     
     def test_item_nome(self):
         response = self.busca(self.sala.nome)
-        self.assertEqual(response['itens'][0]['nome'], self.sala.nome)
-        self.assertEqual(len(response['pessoas']), 0)
+        self.assertEqual(self.sala.nome, response['itens'][0]['nome'])
+        self.assertEqual(0, len(response['pessoas']))
 
     def test_item_codigo(self):
         response = self.busca(self.sala.codigo)
-        self.assertEqual(response['itens'][0]['nome'], self.sala.nome)
-        self.assertEqual(len(response['itens']), 1)
+        self.assertEqual(self.sala.nome, response['itens'][0]['nome'])
+        self.assertEqual(1, len(response['itens']))
 
     def test_item_palavra_chave(self):
         response = self.busca(self.sala.palavras_chave.split(' ')[0])
-        self.assertEqual(response['itens'][0]['nome'], self.sala.nome)
+        self.assertEqual(self.sala.nome, response['itens'][0]['nome'])
 
 
 class TestBuscaItemUnitario(TestCaseBase):
@@ -87,7 +87,7 @@ class TestBuscaItemUnitario(TestCaseBase):
     def test_reserva_futura(self):
         self.sala.fazer_reserva(self.pessoa, self.amanha_hora_1, self.amanha_hora_2,)
         response = self.busca_sala()  
-        self.assertEqual(response['itens'][0]['status']['code'], STATUS_ITEM['disponivel']['code'])
+        self.assertEqual(STATUS_ITEM['disponivel']['code'], response['itens'][0]['status']['code'])
 
     def test_reservado_necessita_confirmar_retirada_e_nao_retirou(self):
         self.sala.necessita_confirmacao_retirada = True
@@ -95,7 +95,7 @@ class TestBuscaItemUnitario(TestCaseBase):
         self.sala.save()
         self.sala.fazer_reserva(self.pessoa, self.hoje_hora_1, self.hoje_hora_2,)
         response = self.busca_sala()  
-        self.assertEqual(response['itens'][0]['status']['code'], STATUS_ITEM['ret_atrasada']['code'])
+        self.assertEqual(STATUS_ITEM['ret_atrasada']['code'], response['itens'][0]['status']['code'])
     
     def test_reservado_e_nao_utilizado___necessita_confirmar_retirada_e_devolucao_e_nao_retirou(self):
         self.sala.necessita_confirmacao_retirada = True
@@ -103,7 +103,7 @@ class TestBuscaItemUnitario(TestCaseBase):
         self.sala.save()
         self.sala.fazer_reserva(self.pessoa, self.hoje_hora_1, self.hoje_hora_2,)
         response = self.busca_sala() 
-        self.assertEqual(response['itens'][0]['status']['verbose'], STATUS_ITEM['ret_atrasada']['verbose'])
+        self.assertEqual(STATUS_ITEM['ret_atrasada']['verbose'], response['itens'][0]['status']['verbose'])
    
     
     def test_reservado_bloqueio_alerta(self):
@@ -111,7 +111,7 @@ class TestBuscaItemUnitario(TestCaseBase):
         self.sala.save()
         self.sala.fazer_reserva(self.pessoa, self.hoje_hora_2, self.hoje_hora_3,)
         response = self.busca_sala()  
-        self.assertEqual(response['itens'][0]['status']['code'], STATUS_ITEM['reservado']['code'])
+        self.assertEqual(STATUS_ITEM['reservado']['code'], response['itens'][0]['status']['code'])
 
     def test_utilizado_e_nao_necessita_confirmar_retirada(self):
         self.sala.necessita_confirmacao_retirada = False
@@ -119,81 +119,81 @@ class TestBuscaItemUnitario(TestCaseBase):
         self.sala.save()
         self.sala.fazer_reserva(self.pessoa, self.hoje_hora_1, self.hoje_hora_2,)
         response = self.busca_sala()  
-        self.assertEqual(response['itens'][0]['status']['code'], STATUS_ITEM['utilizado']['code'])
+        self.assertEqual(STATUS_ITEM['utilizado']['code'], response['itens'][0]['status']['code'])
     
     def test_reservado_precisa_devolver_e_nao_devolveu(self):
         self.sala.fazer_reserva(self.pessoa, self.ontem_hora_1, self.ontem_hora_2,)
         response = self.busca_sala()  
-        self.assertEqual(response['itens'][0]['status']['code'], STATUS_ITEM['devol_atrasada']['code'])
+        self.assertEqual(STATUS_ITEM['devol_atrasada']['code'], response['itens'][0]['status']['code'])
     
     def test_reservado_precisa_devolver_e_devolveu(self):
         reservas = self.sala.fazer_reserva(self.pessoa, self.ontem_hora_1, self.ontem_hora_2,)
         reservas[0].fazer_devolucao()
         response = self.busca_sala()  
-        self.assertEqual(response['itens'][0]['status']['code'], STATUS_ITEM['disponivel']['code'])
+        self.assertEqual(STATUS_ITEM['disponivel']['code'], response['itens'][0]['status']['code'])
     
     def test_reservado_nao_precisa_devolver(self):
         self.sala.necessita_confirmacao_devolucao = False
         self.sala.save()
         self.sala.fazer_reserva(self.pessoa, self.ontem_hora_1, self.ontem_hora_2,)
         response = self.busca_sala()  
-        self.assertEqual(response['itens'][0]['status']['code'], STATUS_ITEM['disponivel']['code'])
+        self.assertEqual(STATUS_ITEM['disponivel']['code'], response['itens'][0]['status']['code'])
         
     def test_fila(self):
         self.sala.reserva_por_fila = True
         self.sala.save()
         self.sala.fazer_reserva(self.pessoa,)
         response = self.busca_sala()  
-        self.assertEqual(response['itens'][0]['status']['code'], STATUS_ITEM['fila']['code'])
+        self.assertEqual(STATUS_ITEM['fila']['code'], response['itens'][0]['status']['code'])
     
     def test_ja_escolhido(self):
         response = self.busca(self.sala.codigo, self.sala.id)
         print (response)
-        self.assertEqual(len(response['itens']), 0)
+        self.assertEqual(0, len(response['itens']))
 
 class TesteBuscaItemMulti(TestCaseBase):
        
     def test_item(self):
         response = self.busca("LAP")
-        self.assertEqual(response['itens'][0]['nome'], self.lapis.nome)
-        self.assertEqual(response['itens'][0]['status']['utilizado'], 0)
-        self.assertEqual(response['itens'][0]['status']['restante'], 3)
-        self.assertEqual(response['itens'][0]['status']['code'], 10)
+        self.assertEqual(self.lapis.nome, response['itens'][0]['nome'])
+        self.assertEqual(0, response['itens'][0]['status']['utilizado'])
+        self.assertEqual(3, response['itens'][0]['status']['restante'])
+        self.assertEqual(10, response['itens'][0]['status']['code'])
 
     def test_item_pedir_agora(self):
         self.lapis.pedir_agora(self.pessoa)
         response = self.busca_lapis()      
-        self.assertEqual(response['itens'][0]['status']['utilizado'], 1)
-        self.assertEqual(response['itens'][0]['status']['restante'], 2)
-        self.assertEqual(response['itens'][0]['status']['attr_class'], 'success')
+        self.assertEqual(1, response['itens'][0]['status']['utilizado'])
+        self.assertEqual(2, response['itens'][0]['status']['restante'])
+        self.assertEqual('success', response['itens'][0]['status']['attr_class'])
       
         self.lapis.pedir_agora(self.pessoa)
         response = self.busca_lapis()
-        self.assertEqual(response['itens'][0]['status']['utilizado'], 2)
-        self.assertEqual(response['itens'][0]['status']['restante'], 1)
-        self.assertEqual(response['itens'][0]['status']['attr_class'], 'success')
+        self.assertEqual(2, response['itens'][0]['status']['utilizado'])
+        self.assertEqual(1, response['itens'][0]['status']['restante'])
+        self.assertEqual('success', response['itens'][0]['status']['attr_class'])
 
         self.lapis.pedir_agora(self.pessoa)
         response = self.busca_lapis()
-        self.assertEqual(response['itens'][0]['status']['utilizado'], 3)
-        self.assertEqual(response['itens'][0]['status']['restante'], 0)
-        self.assertEqual(response['itens'][0]['status']['attr_class'], 'warning')
+        self.assertEqual(3, response['itens'][0]['status']['utilizado'])
+        self.assertEqual(0, response['itens'][0]['status']['restante'])
+        self.assertEqual('warning', response['itens'][0]['status']['attr_class'])
 
     def test_item_pedir_varios_agora(self):
         emprestimos = self.lapis.pedir_agora(self.pessoa, quantidade=3)
         self.assertEqual(len(emprestimos), 3)        
         response = self.busca_lapis()      
-        self.assertEqual(response['itens'][0]['status']['utilizado'], 3)
-        self.assertEqual(response['itens'][0]['status']['restante'], 0)
-        self.assertEqual(response['itens'][0]['status']['attr_class'], 'warning')
+        self.assertEqual(3, response['itens'][0]['status']['utilizado'])
+        self.assertEqual(0, response['itens'][0]['status']['restante'])
+        self.assertEqual('warning', response['itens'][0]['status']['attr_class'])
 
         
     def test_item_devolvido(self):
         emprestimos = self.lapis.pedir_agora(self.pessoa)
         emprestimos[0].fazer_devolucao()
         response = self.busca_lapis()      
-        self.assertEqual(response['itens'][0]['status']['utilizado'], 0)
-        self.assertEqual(response['itens'][0]['status']['restante'], 3)
+        self.assertEqual(0, response['itens'][0]['status']['utilizado'])
+        self.assertEqual(3, response['itens'][0]['status']['restante'])
         
         
     def test_item_varios_devolvidos(self):
@@ -201,5 +201,5 @@ class TesteBuscaItemMulti(TestCaseBase):
         emprestimos[0].fazer_devolucao()
         emprestimos[1].fazer_devolucao()
         response = self.busca_lapis()      
-        self.assertEqual(response['itens'][0]['status']['utilizado'], 1)
-        self.assertEqual(response['itens'][0]['status']['restante'], 2)
+        self.assertEqual(1, response['itens'][0]['status']['utilizado'])
+        self.assertEqual(2, response['itens'][0]['status']['restante'])
